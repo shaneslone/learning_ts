c1cdae19352b3fc493b95ca426d885d19f694d10
Shane Slone <slone.shane@gmail.com>
2021-11-05 11:50:48 -0400

    added functions as well as unknown and never types

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if ((typeof input1 === 'number' && typeof input2 === 'number') ||
-        resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-var combinedNames = combine('Max', 'Anna', 'as-string');
-console.log(combinedNames);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An error occured', 500);
