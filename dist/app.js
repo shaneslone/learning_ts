ded86afb2cdc219651cec29127bdc493124c4c0c
Shane Slone <slone.shane@gmail.com>
2021-11-05 13:36:25 -0400

    changed setting in tsconfig.json

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+const button = document.querySelector('button');
+function clickHandler(message) {
+    console.log('Clicked! ' + message);
+}
+button.addEventListener('click', clickHandler.bind(null, "You're welcome!"));
+//# sourceMappingURL=app.js.map
