60c128ea5c2894f4472f3a593eb8c31de0f0e6cb
Shane Slone <slone.shane@gmail.com>
2021-11-05 12:31:14 -0400

    added tsconfig.json

--- app.ts
+++ app.ts
@@ -1,14 +1,1 @@
-let userInput: unknown;
-let userName: string;
-
-userInput = 5;
-userInput = 'Max';
-if (typeof userInput === 'string') {
-  userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-}
-
-generateError('An error occured', 500);
+console.log('Sending...');
